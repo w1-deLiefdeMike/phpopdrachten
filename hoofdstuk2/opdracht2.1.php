cedb590a1e4c7c3578573b0b67ded516e592afba
Mike de Liefde <mike.deliefde@edu-kw1c.nl>
2020-02-07 13:20:45 +0100

    First try

---
+++ hoofdstuk2/opdracht2.1.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * User: Mike de Liefde
+ * Date: 07-02-2020
+ * Time: 12:00
+ * File: index.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>
+            PHP Template
+        </title>
+        <meta name="description" content="Mijn eerste poging in php">
+        <meta name="keywords" content="plz, help, me">
+        <meta charset="utf-8">
+        <link href="../CSS/style.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+        </header>
+        <p>
+            <?php
+            echo "Hello World!";
+            ?>
+        </p>
+        <main id="wrapper">
+            <h2>Uitwerkingen</h2>
+        </main>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * User: Mike de Liefde
+ * Date: 07-02-2020
+ * Time: 12:00
+ * File: index.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>
+        </title>
+        <meta name="description" content="Mijn eerste poging in php">
+        <meta name="keywords" content="plz, help, me">
+        <meta charset="utf-8"> type="text/css" rel="stylesheet">
+        <link href="CSS/style.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+        </header>
+        <aside>
+            <h2>Menu</h2>
+                <ul>
+                    <li>
+                        <a href="hoofdstuk2/opdracht2.1.php">Opdracht 2.1</a>
+                    </li>
+                    <li>
+                        <!-- <a href="hoofdstuk2/opdracht2.2.php">Opdracht 2.2</a> -->
+                    </li>
+                </ul>
+        </aside>
+        <main id="wrapper">
+            <h2>Uitwerkingen</h2>
+        </main>
+    </body>
+</html>
