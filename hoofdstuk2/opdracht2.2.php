97e6993ad5ac02152e91c59e7753037a15fb090d
Mike de Liefde <mike.deliefde@edu-kw1c.nl>
2020-02-10 11:13:38 +0100

    commit opdracht 2.2

---
+++ hoofdstuk2/opdracht2.2.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * User: Mike de Liefde
+ * Date: 07-02-2020
+ * Time: 12:00
+ * File: index.php
+ */
+?>
+<?php
+    $text1 = "Hallo";
+    $text2 = "een makkelijke taal";
+    $text3 = "toch zo'n makkelijke taal";
+    $text4 = "wat is";
+    $text5 = "PHP";
+    $text6 = "nooit gedacht dat";
+    $text7 = "de installatie is best ingewikkeld";
+    $text8 = "Fijn";
+    $text9 = "?";
+    $text10 = ".";
+    $text11 = ",";
+    $text12 = "<br>";
+    $text13 = "is";
+    $text14 = "vind je niet";
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+        Opdracht 2.2
+    </title>
+    <meta name="description" content="Mijn eerste poging in php">
+    <meta name="keywords" content="plz, help, me">
+    <meta charset="utf-8">
+    <link href="../CSS/style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <p><?php echo "$text1","$text11"," ","$text4"," ","$text5"," ","$text3","$text10","$text12","$text7","$text10"," ","$text8"," toch","$text9","$text12"," ","$text6"," ","$text5"," ","$text3"," ","$text13","$text10"; ?></p>
+</header>
+<p>
+</p>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+</main>
+</body>
+</html>
