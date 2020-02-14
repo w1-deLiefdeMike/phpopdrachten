78e37a3e2576686c208b2227bc12e6fb7fb4c416
Mike de Liefde <mike.deliefde@edu-kw1c.nl>
2020-02-14 12:12:21 +0100

    commit opdracht 3.1

--- index.php
+++ index.php
@@ -27,7 +27,10 @@
                         <a href="hoofdstuk2/opdracht2.1.php">Opdracht 2.1</a>
                     </li>
                     <li>
-                        <!-- <a href="hoofdstuk2/opdracht2.2.php">Opdracht 2.2</a> -->
+                        <a href="hoofdstuk2/opdracht2.2.php">Opdracht 2.2</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk3/opdracht3.1.php">Opdracht 3.1</a>
                     </li>
                 </ul>
         </aside>
