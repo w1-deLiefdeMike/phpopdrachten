3404dfc99ca1782c0f1011ca2ccad28762b87e3a
Mike de Liefde <mike.deliefde@edu-kw1c.nl>
2020-05-29 17:13:26 +0200

    Laatste bugfix, nu werkt het naar behoren.

--- hoofdstuk6/opdracht6.1.php
+++ hoofdstuk6/opdracht6.1.php
@@ -4,12 +4,12 @@ include "../includes/header.php";
 <form action="checkLogin.php" method="post">
     <label>Username: </label>
     <label>
-    <input type="text" name="Username">
+    <input type="text" name="username">
     </label>
     <br>
     <label>Password: </label>
     <label>
-    <input type="password" name="Password">
+    <input type="password" name="password">
     </label>
     <br>
     <input type="submit" value="Verzend">
